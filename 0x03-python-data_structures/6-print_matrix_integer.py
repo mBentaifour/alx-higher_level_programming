62e63fb6f973b3dac8338a5f553a85ce4356be38
mbentaifour <bentaifourmoh@gmail.com>
2024-01-08 12:54:40 -0800

    structures

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        return None
+    for submatrix in matrix:
+        if len(submatrix) == 0:
+            print()
+        for k in range(len(submatrix)):
+            print("{:d}".format(submatrix[k]),
+                    end="\n" if k is len(submatrix) - 1 else " ")
