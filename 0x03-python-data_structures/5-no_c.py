d2b2e51e35eb150a092fa80342f936b57ba29f97
mbentaifour <bentaifourmoh@gmail.com>
2024-01-08 12:31:49 -0800

    structures

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,5 +3,5 @@ def no_c(my_string):
     bac = ""
     for s in range(len(my_string)):
         if (my_string[s] != 'c' and my_string[s] != 'C'):
-                bac += my_string[i]
+                bac += my_string[s]
     return bac
