39be4946a84f447222850dd48d8f44c669ce1fd4
mbentaifour <bentaifourmoh@gmail.com>
2024-01-10 12:23:31 -0800

    More Data Structures

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return list(map(lambda sumbat: list(map(lambda e: e**2, submat)), matrix))
+    squared = []
+    for line in matrix:
+        squared.append([c**2 for c in line])
+    return squared
