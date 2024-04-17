1390d9589ffec73085eafcbb032bf98e1496ae63
mBentaifour <bentaifourmoh@gmail.com>
2024-04-17 16:39:36 -0700

    input/output in python

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-"""function that writes an Object to a text file"""
+
+"""This module saves a Python object to a JSON file"""
+
 import json
 
 
 def save_to_json_file(my_obj, filename):
-    """writes an Object to a text file, using a JSON representation
-
-    Args > my_obj (any) >> Python object // filename (str) >> The name of the file
-    """
-    with open(filename, "w") as file:
-        json.dump(my_obj, file)
+    """saves a Python object to a JSON file"""
+    with open(filename, 'w', encoding='UTF-8') as file:
+        file.write(json.dumps(my_obj))
