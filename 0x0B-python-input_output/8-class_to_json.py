18c57e131e334dcb7572b32c6b3d484229dd3a4f
mBentaifour <bentaifourmoh@gmail.com>
2024-04-17 17:28:38 -0700

    input/output in python

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-"""Defines class_to_json"""
+"""
+Module for JSON serialization
+"""
 
 
 def class_to_json(obj):
-    """function returns the dictionary description with simple data structure"""
+    """ Returns dictionary description with data list """
     return obj.__dict__
