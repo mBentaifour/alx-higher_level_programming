ea5e4c892342b4232f840b9014d05e8754a8e743
mBentaifour <bentaifourmoh@gmail.com>
2024-05-23 16:56:19 -0700

    SQL more

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -2,10 +2,10 @@
 SELECT tv_genres.name
 FROM tv_genres
 LEFT JOIN (
-    SELECT genre_id
-        FROM shows_genres
-	    JOIN tv_shows ON shows_genres.show_id = tv_shows.id
-	        WHERE tv_shows.title = 'Dexter'
-		) AS dexter_genres ON tv_genres.id = dexter_genres.genre_id
-		WHERE dexter_genres.genre_id IS NULL
-		ORDER BY tv_genres.name ASC;
+	SELECT genre_id
+		FROM shows_genres
+			JOIN tv_shows ON shows_genres.show_id = tv_shows.id
+				WHERE tv_shows.title = 'Dexter'
+				) AS dexter_genres ON tv_genres.id = dexter_genres.genre_id
+				WHERE dexter_genres.genre_id IS NULL
+				ORDER BY tv_genres.name ASC;
