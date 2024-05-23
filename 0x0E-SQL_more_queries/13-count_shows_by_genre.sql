9e2848e635a26d0e07ca5706e365bfee93a46c3b
mBentaifour <bentaifourmoh@gmail.com>
2024-05-23 16:43:17 -0700

    SQL more

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- cound and join
+SELECT name AS genre, COUNT(*) AS number_of_shows
+FROM tv_genres
+INNER JOIN tv_show_genres ON id=tv_show_genres.genre_id
+GROUP BY tv_show_genres.genre_id
+ORDER BY number_of_shows DESC;
