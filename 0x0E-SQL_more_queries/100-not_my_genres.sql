1b145c492b3a8605d23ede2f8ac3e5bd2f4df16b
mBentaifour <bentaifourmoh@gmail.com>
2024-05-23 17:01:16 -0700

    SQL more

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,7 +1,7 @@
---  list all genres not linked to the show Dexter
+-- 100-not_my_genres.sql
+--  uses the hbtn_0d_tvshows database to list all genres not linked to the show
+--  Dexter
 SELECT DISTINCT tv_genres.name
-       FROM tv_show_genres
-       LEFT JOIN tv_shows
-       ON tv_show_genres.show_id=tv_shows.id
-       LEFT JOIN tv_genres
-       ON tv_show_genres.genre_id=tv_genres.id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
