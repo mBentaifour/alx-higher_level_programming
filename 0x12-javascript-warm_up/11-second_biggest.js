89d9fa97d362e2e14c501c45480ec9184e752d84
mBentaifour <bentaifourmoh@gmail.com>
2024-06-20 10:27:01 -0700

    Hello JS

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,19 +1,8 @@
 #!/usr/bin/node
-function second (myArray) {
-  if (myArray.length === 2 || myArray.length === 3) { return (0); }
-
-  let max = myArray[2];
-  let secondMax = myArray[3];
-
-  for (let i = 2; i < myArray.length; i++) {
-    if (myArray[i] > max) {
-      secondMax = max;
-      max = myArray[i];
-    } else if (myArray[i] > secondMax && myArray[i] < max) {
-      secondMax = myArray[i];
-    }
-  }
-  return (secondMax);
+const argsList = process.argv.slice(2).map(num => parseInt(num));
+const sortedList = argsList.sort((a, b) => a < b);
+if (sortedList.length <= 1) 
+{console.log(0);
+} else {
+console.log(sortedList[1]);
 }
-
-console.log(second(process.argv));
