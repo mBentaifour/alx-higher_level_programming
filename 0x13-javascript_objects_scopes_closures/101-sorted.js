79212904078f4fd9c88837a7cfb09939fd2775be
mBentaifour <bentaifourmoh@gmail.com>
2024-06-21 12:40:59 -0700

    JS objects and scopes

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -6,9 +6,10 @@
 const dictionary = require('./101-data.js').dict;
 let newdict = {};
 for (let key in dictionary) {
-  if (newdict[dictionary[key]] === undefined) {
-    newdict[dictionary[key]] = [];
-}
-newdict[dictionary[key]].push(key);
+	if (newdict[dictionary[key]] === undefined) {
+		newdict[dictionary[key]] = [];
+	}
+	newdict[dictionary[key]].push(key);
 }
 console.log(newdict);
+
