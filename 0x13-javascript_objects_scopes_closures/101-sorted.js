15173e1d7b8539583f6b51de17e836c8ed4c3926
mBentaifour <bentaifourmoh@gmail.com>
2024-06-21 12:38:10 -0700

    JS objects and scopes

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -8,7 +8,7 @@ let newdict = {};
 for (let key in dictionary) {
   if (newdict[dictionary[key]] === undefined) {
     newdict[dictionary[key]] = [];
-  }
+}
 newdict[dictionary[key]].push(key);
 }
 console.log(newdict);
