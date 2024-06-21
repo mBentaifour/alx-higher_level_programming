6b79ee3458f1cdb711c4e90fb225c5b9789b8646
mBentaifour <bentaifourmoh@gmail.com>
2024-06-21 11:19:49 -0700

    JS objects and scopes

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
+
 // Write a class Rectangle that defines a rectangle
 // use the "class" notation for defining your class
-//The constructor must take 2 arguments w and h
-//Initialize the instance attribute width with the value of w
-//Initialize the instance attribute height with the value of h
+// The constructor must take 2 arguments w and h
+// Initialize the instance attribute width with the value of w
+// Initialize the instance attribute height with the value of h
 
 class Rectangle {
   constructor (w, h) {
