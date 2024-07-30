e324f95ff50a9e2d29ce073c336507118982960c
mBentaifour <bentaifourmoh@gmail.com>
2024-07-30 11:17:12 -0700

    JS scraping

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -4,25 +4,18 @@
 // prints all characters of a Star Wars movie
 
 const request = require('request');
-const id = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
-
-request.get(url, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    const content = JSON.parse(body);
-    const characters = content.characters;
-    // console.log(characters);
-    for (const character of characters) {
-      request.get(character, (error, response, body) => {
-        if (error) {
-          console.log(error);
-        } else {
-          const names = JSON.parse(body);
-          console.log(names.name);
-        }
-      });
-    }
-  }
+request.get(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`, (err, response, body) => {
+  if (err) console.log(err);
+  logCharacter(JSON.parse(body).characters, 0);
 });
+
+function logCharacter (characters, i) {
+  if (i > characters.length - 1) {
+    return;
+  }
+  request.get(characters[i], (error, res, data) => {
+    if (error) console.log(error);
+    console.log(JSON.parse(data).name);
+    logCharacter(characters, i + 1);
+  });
+}
